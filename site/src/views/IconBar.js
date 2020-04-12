11325ae3a6d823e56bf7e4cb97b241339b2d7ed7
gracielawp <gracielawp@users.noreply.github.com>
2020-04-12 23:00:21 +0100

    working version of icons on record rotating

--- site/src/views/IconBar.js
+++ site/src/views/IconBar.js
@@ -1,45 +1,61 @@
 import React from 'react';
-import './IconBar.css';
+import style from './IconBar.module.css';
 import YouTubeIcon from '@material-ui/icons/YouTube';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import EmailIcon from '@material-ui/icons/Email';
 import Icon from '@material-ui/core/Icon';
 
+function StyledIcon(props) {
+    return (
+        <li>
+            <a href={props.link}>
+                <props.icon className={style.icon} />
+            </a>
+        </li>
+    );
+}
+
+function StyledFaIcon(props) {
+    return(
+        <li>
+            <a href={props.link}>
+                <div className={style.icon}>
+                    <Icon className={props.icon} />
+                </div>
+            </a>
+        </li>
+    );
+}
+
 function IconBar() {
     return (
-        <div class="footer">
+        <div className={style.footer}>
             <ul>
-                <li>
-                    <a href="https://www.instagram.com/tangerinerye/">
-                        <InstagramIcon /> 
-                    </a>   
-                </li>
-                <li>
-                    <a href="https://www.facebook.com/tangerinerye/">
-                        <FacebookIcon />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.youtube.com/channel/UC18c12v12WXT4X-yljzwZwg">
-                        <YouTubeIcon />
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <EmailIcon />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://open.spotify.com/artist/6N7HQGSTngvAUUqDquoXfI">
-                        <Icon className="fab fa-spotify" />
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <Icon className="fab fa-itunes-note" />
-                    </a>
-                </li>
+                <StyledIcon
+                    link="https://www.instagram.com/tangerinerye/"
+                    icon={InstagramIcon}
+                />
+                <StyledIcon
+                    link="https://www.facebook.com/tangerinerye/"
+                    icon={FacebookIcon}
+                />
+                <StyledIcon
+                    link="https://www.youtube.com/channel/UC18c12v12WXT4X-yljzwZwg"
+                    icon={YouTubeIcon}
+                />
+                <StyledIcon
+                    link=""
+                    icon={EmailIcon}
+                />
+                <StyledFaIcon
+                    link="https://open.spotify.com/artist/6N7HQGSTngvAUUqDquoXfI"
+                    icon="fab fa-spotify"
+                />
+                <StyledFaIcon
+                    link="https://music.apple.com/gb/artist/tangerine-rye/id1441879382"
+                    icon="fab fa-itunes-note"
+                />
             </ul>
         </div>
     );
