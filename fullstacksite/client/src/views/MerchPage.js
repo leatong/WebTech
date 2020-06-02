86b381724ff9b992cc2bee88d1a75311856091c2
leatong <leatong95ts@gmail.com>
2020-06-02 16:49:00 +0100

    added about page

--- fullstacksite/client/src/views/MerchPage.js
+++ fullstacksite/client/src/views/MerchPage.js
@@ -4,26 +4,33 @@ import merch from './MerchPage.module.css';
 import Tag from "../Component/Tag";
 import Tshirt1 from "../assets/Tshirt1-square.jpg";
 import Album1 from "../assets/album1.png";
-import Album2 from "../assets/honey.png";
+import single1 from "../assets/honey.png";
+import single2 from "../assets/lalalemon.png";
 
 function MerchPage(){
     return (
         <div>
             <OverHeadBar currentPage='SHOP'/>
-                <div className={merch.overview}>
-                    <div className={merch.tile}>
-                        <img className={merch.pic} src={Tshirt1} alt='White Tee'/>
-                        <Tag name='tshirt1' description='White Tee' price='£ 5.99'/>
-                    </div>
-                    <div className={merch.tile}>
-                        <img className={merch.pic} src={Album1} alt='Album1 Cover'/>
-                        <Tag name='album1' description='Album1' price='£ 4.99'/>
-                    </div>
-                    <div className={merch.tile}>
-                        <img className={merch.pic} src={Album2} alt='Album2 Cover'/>
-                        <Tag name='album2' description='Album2' price='£ 5.99'/>
-                    </div>
+            <div className={merch.overview}>
+                <div className={merch.tile}>
+                    <img className={merch.pic} src={Album1} alt='Album1 Cover'/>
+                    <Tag name='album1' description='Tangerine Rye - Album' price='£ 2.99'/>
                 </div>
+                <div className={merch.tile}>
+                    <img className={merch.pic} src={single1} alt='Single1 Cover'/>
+                    <Tag name='single1' description='La La Lemon - Single' price='£ 0.99'/>
+                </div>
+                <div className={merch.tile}>
+                    <img className={merch.pic} src={single2} alt='Single2 Cover'/>
+                    <Tag name='single2' description='Honey - Single' price='£ 0.99'/>
+                </div>
+            </div>
+            <div className={merch.overview}>
+                <div className={merch.tile}>
+                    <img className={merch.pic} src={Tshirt1} alt='White Tee'/>
+                    <Tag name='tshirt1' description='White Tee' price='£ 3.99'/>
+                </div>
+            </div>
         </div>
     );
 }
